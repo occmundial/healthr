0b51b083d433f30adc0c1e92d26d99e2aae656f4
AbrJA <ajaimes@occ.com.mx>
2024-05-15 11:19:57 -0600

    Better alert structure

--- R/alert.R
+++ R/alert.R
@@ -8,8 +8,13 @@
 Alert <- R6::R6Class(
   classname = "Alert",
   public = list(
-    send = function(text,
-                    url = Sys.getenv("SLACK_URL"),
+    build = function(header, section, color) {
+      header <- list(type = "header", text = list(type = "plain_text", text = header))
+      section <- list(type = "section", text = list(type = "mrkdwn", text = section))
+      private$.message <- list(color = color, blocks = list(private$.header, private$.section))
+      invisible(self)
+    },
+    send = function(url = Sys.getenv("SLACK_URL"),
                     token = Sys.getenv("SLACK_TOKEN"),
                     channel = Sys.getenv("SLACK_CHANNEL"),
                     username = Sys.getenv("SLACK_USERNAME"),
@@ -18,11 +23,18 @@ Alert <- R6::R6Class(
       checkmate::assertString(token)
       checkmate::assertString(channel)
       checkmate::assertString(username)
-      body <- list(channel = channel, username = username, text = text, icon_emoji = emoji)
+      if (checkmate::testNull(private$.message)) stop("Build the alert first")
+      body <- list(channel = channel,
+                   username = username,
+                   icon_emoji = emoji,
+                   attachments = list(private$.message))
       httr::POST(url = url, httr::content_type_json(),
                  httr::add_headers(Authorization = token),
                  body = yyjsonr::write_json_str(body, opts = list(auto_unbox = TRUE)))
     }
+  ),
+  active = list(
+    message = function() private$.message
   ),
   private = list(
     .message = NULL
